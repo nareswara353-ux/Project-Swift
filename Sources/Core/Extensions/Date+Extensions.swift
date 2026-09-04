8bca67580aacd50a3e5d82301c78a3a7799de1a2
Narezzzs <nareswara353@gmail.com>
2026-09-05 00:33:00 +0700

    feat(core): add Date extensions for ISO8601 formatting

---
+++ Sources/Core/Extensions/Date+Extensions.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+extension Date {
+    func iso8601String() -> String {
+        let formatter = ISO8601DateFormatter()
+        formatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+        return formatter.string(from: self)
+    }
+    
+    static func fromISO8601(_ string: String) -> Date? {
+        let formatter = ISO8601DateFormatter()
+        formatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+        return formatter.date(from: string)
+    }
+}

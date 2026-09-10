e5b283620d9ad1e0d5921aa2a0f5169852b9f529
Narezzzs <nareswara353@gmail.com>
2026-09-11 05:41:31 +0700

    fix(core): convert String to Logger.Message via string interpolation

--- Sources/Core/Logging/AppLogger.swift
+++ Sources/Core/Logging/AppLogger.swift
@@ -13,31 +13,31 @@ public struct AppLogger: Sendable {
     }
     
     public func trace(_ message: @autoclosure () -> String, file: String = #file, function: String = #function, line: Int = #line) {
-        logger.trace(message(), metadata: metadata(file: file, function: function, line: line))
+        logger.trace("\(message())", metadata: metadata(file: file, function: function, line: line))
     }
     
     public func debug(_ message: @autoclosure () -> String, file: String = #file, function: String = #function, line: Int = #line) {
-        logger.debug(message(), metadata: metadata(file: file, function: function, line: line))
+        logger.debug("\(message())", metadata: metadata(file: file, function: function, line: line))
     }
     
     public func info(_ message: @autoclosure () -> String, file: String = #file, function: String = #function, line: Int = #line) {
-        logger.info(message(), metadata: metadata(file: file, function: function, line: line))
+        logger.info("\(message())", metadata: metadata(file: file, function: function, line: line))
     }
     
     public func notice(_ message: @autoclosure () -> String, file: String = #file, function: String = #function, line: Int = #line) {
-        logger.notice(message(), metadata: metadata(file: file, function: function, line: line))
+        logger.notice("\(message())", metadata: metadata(file: file, function: function, line: line))
     }
     
     public func warning(_ message: @autoclosure () -> String, file: String = #file, function: String = #function, line: Int = #line) {
-        logger.warning(message(), metadata: metadata(file: file, function: function, line: line))
+        logger.warning("\(message())", metadata: metadata(file: file, function: function, line: line))
     }
     
     public func error(_ message: @autoclosure () -> String, file: String = #file, function: String = #function, line: Int = #line) {
-        logger.error(message(), metadata: metadata(file: file, function: function, line: line))
+        logger.error("\(message())", metadata: metadata(file: file, function: function, line: line))
     }
     
     public func critical(_ message: @autoclosure () -> String, file: String = #file, function: String = #function, line: Int = #line) {
-        logger.critical(message(), metadata: metadata(file: file, function: function, line: line))
+        logger.critical("\(message())", metadata: metadata(file: file, function: function, line: line))
     }
     
     private func metadata(file: String, function: String, line: Int) -> Logger.Metadata {
